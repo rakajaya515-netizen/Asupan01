44dbee5f66fe4e57fe6e2408ef8b08d5e3846593
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-27 21:59:29 +0700

    Change video API endpoint to local path

--- api.videos.js
+++ api.videos.js
@@ -2,9 +2,7 @@ export default async function handler(req, res) {
   try {
     const API_KEY = process.env.VIDARA_API_KEY;
 
-    const response = await fetch(
-      `https://api.vidara.so/v1/video/list?api_key=${API_KEY}`
-    );
+    const response = await fetch("/api/videos");
 
     const data = await response.json();
 
