ac6385cd714bc9036bb7f6932b515ce0d8027377
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-27 22:07:37 +0700

    Update video API endpoint to use Vidara API

--- api.videos.js
+++ api.videos.js
@@ -2,7 +2,9 @@ export default async function handler(req, res) {
   try {
     const API_KEY = process.env.VIDARA_API_KEY;
 
-    const response = await fetch("/api/videos");
+    const response = await fetch(
+      `https://api.vidara.so/v1/file/list?api_key=${API_KEY}`
+    );
 
     const data = await response.json();
 
