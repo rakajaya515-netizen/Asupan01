2511351928c80e0656b46c47f4ff422aafa808b1
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 15:31:04 +0700

    Add RootLayout component with metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Asupanmu",
+  description: "Streaming Video"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
