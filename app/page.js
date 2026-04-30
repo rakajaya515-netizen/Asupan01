3eb5149c4f45b1f8aed190f9bfe50a316f564b46
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 15:50:50 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -17,47 +17,66 @@ export default function Home() {
   );
 
   return (
-    <div style={{ padding: "20px", background: "#000", color: "#fff" }}>
-      <h1 style={{ fontSize: "28px", marginBottom: "10px" }}>
-        Asupanmu01
-      </h1>
+    <div style={{ padding: "10px", background: "#000", minHeight: "100vh" }}>
+      <h1 style={{ color: "#fff" }}>Asupanmu</h1>
 
       <input
-        type="text"
-        placeholder="Search video..."
+        placeholder="Search..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         style={{
           width: "100%",
           padding: "10px",
-          borderRadius: "10px",
-          marginBottom: "20px",
-          border: "none"
+          borderRadius: "20px",
+          margin: "10px 0"
         }}
       />
 
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "1fr",
-          gap: "20px"
+          gridTemplateColumns: "1fr 1fr",
+          gap: "12px"
         }}
       >
         {filtered.map((v, i) => (
-          <div key={i}>
-            <img
-              src={v.thumbnail}
-              style={{ width: "100%", borderRadius: "10px" }}
-            />
-            <p>{v.title}</p>
+          <a
+            key={i}
+            href={v.video_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            <div
+              style={{
+                position: "relative",
+                borderRadius: "16px",
+                overflow: "hidden"
+              }}
+            >
+              <img
+                src={v.thumbnail}
+                style={{
+                  width: "100%",
+                  height: "220px",
+                  objectFit: "cover"
+                }}
+              />
 
-            <iframe
-              src={v.video_url}
-              width="100%"
-              height="200"
-              allowFullScreen
-            ></iframe>
-          </div>
+              <div
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  width: "100%",
+                  padding: "10px",
+                  background:
+                    "linear-gradient(to top, rgba(0,0,0,0.8), transparent)"
+                }}
+              >
+                <p style={{ fontSize: "13px" }}>{v.title}</p>
+              </div>
+            </div>
+          </a>
         ))}
       </div>
     </div>
