dd8d7993f464ada1a281a935d550dc762f0eab66
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 21:52:25 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,42 @@
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self'; img-src * data:; media-src *; script-src 'self' 'unsafe-inline'; frame-src *;"
-  },
-  { key: "X-Frame-Options", value: "SAMEORIGIN" },
-  { key: "X-Content-Type-Options", value: "nosniff" },
-  { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload"
-  }
-];
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
         source: "/(.*)",
-        headers: securityHeaders
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              img-src 'self' data: https:;
+              media-src 'self' https:;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
+              style-src 'self' 'unsafe-inline' https:;
+              connect-src 'self' https:;
+              frame-src *;
+            `.replace(/\n/g, "")
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN"
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          }
+        ]
       }
     ];
   }
 };
+
+module.exports = nextConfig;
