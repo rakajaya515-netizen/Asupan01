8655d6c4522ea4d3d031a36c70f8065568fa6d3f
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 18:59:52 +0700

    Update RootLayout to include font and metadata

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,21 @@
+import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400','600','700']
+})
+
 export const metadata = {
-  title: "Asupanmu"
-};
+  title: 'Asupanmu'
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={poppins.className}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
