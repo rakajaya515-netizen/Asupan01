c08484c35e52b01a7ce2fdca07b4caa930ba3a86
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 20:00:01 +0700

    Implement security headers in next.config.js
    Added security headers to enhance application security.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self'; img-src * data:; media-src *; script-src 'self' 'unsafe-inline'; frame-src *;"
+  },
+  { key: "X-Frame-Options", value: "SAMEORIGIN" },
+  { key: "X-Content-Type-Options", value: "nosniff" },
+  { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  }
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders
+      }
+    ];
   }
 };
-
-module.exports = nextConfig;
