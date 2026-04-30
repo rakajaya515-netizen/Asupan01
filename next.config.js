7f8149fe1ee3c3fc7a81bea88d057689c9b4643f
rakajaya515-netizen <rakajaya515@gmail.com>
2026-04-30 21:56:56 +0700

    Modify security headers in next.config.js
    Updated Content Security Policy and removed several security headers.

--- next.config.js
+++ next.config.js
@@ -8,30 +8,8 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-              default-src 'self';
-              img-src 'self' data: https:;
-              media-src 'self' https:;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-              style-src 'self' 'unsafe-inline' https:;
-              connect-src 'self' https:;
-              frame-src *;
+              default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
             `.replace(/\n/g, "")
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN"
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff"
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin"
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload"
           }
         ]
       }
