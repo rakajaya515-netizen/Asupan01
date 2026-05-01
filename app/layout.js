f92f6b64d1f563873e81266bd1d2953ca7342e64
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-01 22:57:02 +0700

    Refactor layout.js to simplify imports and body structure

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,13 @@
-import './globals.css'
-import { Poppins } from 'next/font/google'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400','600','700']
-})
+import "./globals.css";
 
 export const metadata = {
-  title: 'Asupanmu'
-}
+  title: "Asupanmu",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
