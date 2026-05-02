56166ef69fd789f1f63efd1fa704dd722ea85a22
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 08:37:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self' * data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' *; style-src 'self' 'unsafe-inline' *; img-src * data: blob:; media-src * blob:; frame-src *;",
-  },
-  { key: "X-Frame-Options", value: "SAMEORIGIN" },
-  { key: "X-Content-Type-Options", value: "nosniff" },
-  { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-];
-
-module.exports = {
-  reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ];
+const nextConfig = {
+  images: {
+    domains: ["i.doodcdn.com"],
   },
 };
+
+module.exports = nextConfig;
