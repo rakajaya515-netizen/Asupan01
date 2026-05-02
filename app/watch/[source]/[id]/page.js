3b043cd7b164f7a9e722a320c05f832f7a14b2ff
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 15:15:01 +0700

    Update and rename app/watch/page.js to app/watch/[source]/[id]/page.js

---
+++ app/watch/[source]/[id]/page.js
@@ -0,0 +1,32 @@
+export async function generateMetadata({ params }) {
+  return {
+    title: `Watch ${params.id} - Asupanmu`,
+  };
+}
+
+export default function Watch({ params }) {
+  const { id, source } = params;
+
+  let embed = "";
+
+  if (source === "dood") {
+    embed = `https://doodstream.com/e/${id}`;
+  }
+
+  if (source === "vidara") {
+    embed = `https://vidara.so/${id}`;
+  }
+
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Watch Video</h1>
+
+      <iframe
+        src={embed}
+        width="100%"
+        height="500"
+        allowFullScreen
+      ></iframe>
+    </main>
+  );
+}
