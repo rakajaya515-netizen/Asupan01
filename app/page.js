3113a89b2248a39c9688ecc48807df6f7ce5983f
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 14:16:07 +0700

    Update video rendering and styling in page.js
    Refactor video display to use anchor tags and improve styling.

--- app/page.js
+++ app/page.js
@@ -7,27 +7,51 @@ export default function Home() {
 
   useEffect(() => {
     fetch("/api/videos")
-      .then(res => res.json())
-      .then(data => {
-        console.log("DATA:", data);
-        setVideos(data);
-      });
+      .then((res) => res.json())
+      .then((data) => setVideos(data));
   }, []);
 
   return (
-    <div style={{ padding: 16 }}>
+    <div style={{ padding: 16, background: "black", minHeight: "100vh" }}>
       <h1 style={{ color: "white" }}>Asupanmu</h1>
 
-      <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(2, 1fr)",
-        gap: 10
-      }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: 10,
+        }}
+      >
         {videos.map((v, i) => (
-          <div key={i}>
-            <img src={v.thumbnail} style={{ width: "100%" }} />
-            <p style={{ color: "white" }}>{v.title}</p>
-          </div>
+          <a key={i} href={v.url} style={{ textDecoration: "none" }}>
+            <div
+              style={{
+                width: "100%",
+                aspectRatio: "9/16",
+                overflow: "hidden",
+                borderRadius: 8,
+              }}
+            >
+              <img
+                src={v.thumbnail}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+
+            <p
+              style={{
+                color: "white",
+                fontSize: 14,
+                marginTop: 5,
+              }}
+            >
+              {v.title}
+            </p>
+          </a>
         ))}
       </div>
     </div>
