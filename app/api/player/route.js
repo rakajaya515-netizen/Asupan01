edb4db937a801486b378bc308afe1c02d41a4154
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 08:55:32 +0700

    Update route.js

--- app/api/player/route.js
+++ app/api/player/route.js
@@ -1,21 +1,25 @@
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
+
   const source = searchParams.get("source");
   const id = searchParams.get("id");
 
-  let url = "";
-
-  if (!id || !source) {
-    return Response.json({ url: "" });
+  if (!source || !id) {
+    return Response.json({ error: "Missing params" }, { status: 400 });
   }
 
+  let url = "";
+
   if (source === "dood") {
-    // domain cepat & stabil
-    url = `https://dood.cx/e/${id}`;
+    url = `https://doodstream.com/e/${id}`;
   }
 
   if (source === "vidara") {
     url = `https://vidara.so/embed/${id}`;
+  }
+
+  if (!url) {
+    return Response.json({ error: "Invalid source" }, { status: 400 });
   }
 
   return Response.json({ url });
