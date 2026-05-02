70474cc4756f58232bfd97e5dbefec6aa6fa1f85
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-03 01:51:32 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,30 +1,34 @@
 export default async function handler(req, res) {
-  const API_KEY = "05986baf0f6ed182421e1c9733a698e6c69fa36fe6c0ebbec23611f4950e9721";
+  const API_KEY = process.env.API_KEY;
+  const { code } = req.query;
 
   if (!API_KEY) {
     return res.status(500).json({ error: "API KEY kosong" });
   }
 
+  if (!code) {
+    return res.status(400).json({ error: "Code kosong" });
+  }
+
   try {
-    const url = `https://api.vidara.so/v1/video/list?api_key=${API_KEY}&limit=20`;
+    const url = `https://api.vidara.so/v1/video/info?api_key=${API_KEY}&filecode=${code}`;
 
     const response = await fetch(url);
 
     if (!response.ok) {
       return res.status(500).json({
         error: "Fetch gagal",
-        status: response.status
+        status: response.status,
       });
     }
 
     const data = await response.json();
 
     return res.status(200).json(data);
-
   } catch (error) {
     return res.status(500).json({
       error: "Server error",
-      message: error.message
+      message: error.message,
     });
   }
 }
