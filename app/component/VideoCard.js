509aa22c7bbd55b4f3efdae76b25aab74d4e3b62
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 15:12:57 +0700

    Create VideoCard.js

---
+++ app/component/VideoCard.js
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+
+export default function VideoCard({ video }) {
+  return (
+    <Link href={`/watch/${video.source}/${video.id}`}>
+      <div className="card">
+        <img src={video.thumbnail} alt={video.title} />
+      </div>
+
+      <style jsx>{`
+        .card {
+          cursor: pointer;
+        }
+        img {
+          width: 100%;
+          height: 200px;
+          object-fit: cover;
+          border-radius: 8px;
+        }
+      `}</style>
+    </Link>
+  );
+}
