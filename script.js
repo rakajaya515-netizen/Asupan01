2ea4d58d0c7fb6f22b60aac47993592a523b1f21
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-03 01:19:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const API_KEY = 2b411a6b899373e60acb55c96a682f6506946723cdeaa537512c47c50a2dbac7;
-
-// endpoint list video
 const API_URL = `https://api.vidara.so/v1/video/list?api_key=${API_KEY}&limit=20`;
 
 async function loadVideos() {
+  const container = document.getElementById("videoList");
+
+  container.innerHTML = "<p>Loading...</p>";
+
   try {
     const res = await fetch(API_URL);
     const data = await res.json();
 
-    const container = document.getElementById("videoList");
+    console.log("API RESPONSE:", data);
+
+    if (!data.result || !data.result.videos) {
+      container.innerHTML = "<p>Gagal ambil data</p>";
+      return;
+    }
+
+    container.innerHTML = "";
 
     data.result.videos.forEach(video => {
       const card = document.createElement("div");
@@ -16,10 +25,9 @@ async function loadVideos() {
 
       card.innerHTML = `
         <img src="${video.thumbnail}" />
-        <div class="title">${video.title}</div>
+        <div class="title">${video.title || "No Title"}</div>
       `;
 
-      // klik → pindah ke halaman resmi
       card.onclick = () => {
         window.location.href = `https://vidara.so/${video.filecode}`;
       };
@@ -29,6 +37,7 @@ async function loadVideos() {
 
   } catch (err) {
     console.error(err);
+    container.innerHTML = "<p>Error load API</p>";
   }
 }
 
