27e656eb20d24a993341ee6d52d9412d0c9fc0d0
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-02 08:35:20 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
 
-export const dynamic = "force-dynamic";
-
 async function getVideos() {
-  const res = await fetch("https://asupan01.vercel.app/api/videos", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/videos`, {
     cache: "no-store",
   });
+
   return res.json();
 }
 
@@ -13,16 +12,12 @@ export default async function Home() {
   const videos = await getVideos();
 
   return (
-    <main className="container">
-      <h1 className="title">Asupanmu</h1>
+    <main style={{ padding: 20 }}>
+      <h1>Asupanmu</h1>
 
       <div className="grid">
         {videos.map((v, i) => (
-          <Link
-            key={i}
-            href={`/watch/${v.source}/${v.id}`}
-            className="card"
-          >
+          <Link key={i} href={v.url} className="card">
             <img src={v.thumbnail} />
             <p>{v.title}</p>
           </Link>
