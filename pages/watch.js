19188b6101b32fa16f9547b1cd174fe338e2de98
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-05 00:16:49 +0700

    Update watch.js

--- pages/watch.js
+++ pages/watch.js
@@ -1,31 +1,14 @@
 import { useRouter } from "next/router"
 
 export default function Watch() {
-  const router = useRouter()
-  const { filecode } = router.query
+  if (typeof window !== "undefined") {
+    const params = new URLSearchParams(window.location.search);
+    const url = params.get("url");
 
-  if (!filecode) return <p>Loading...</p>
+    if (url) {
+      window.location.href = url;
+    }
+  }
 
-  const videoUrl = `https://vidara.so/${filecode}`
-
-  return (
-    <div style={{
-      background: "#000",
-      color: "#fff",
-      minHeight: "100vh",
-      padding: 20
-    }}>
-      <h2>Video Player</h2>
-
-      <div style={{ marginTop: 20 }}>
-        <iframe
-          src={videoUrl}
-          width="100%"
-          height="500"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
-    </div>
-  )
+  return <p style={{ color: "white" }}>Redirecting...</p>;
 }
