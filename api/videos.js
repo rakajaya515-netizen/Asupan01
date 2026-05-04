f0d8beb39395989793068495e3f60efb546f8dc3
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-05 00:37:21 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,15 +1,17 @@
 export default function handler(req, res) {
+  res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+
   res.status(200).json([
     {
       id: 1,
       title: "Test Video 1",
-      thumbnail: "https://picsum.photos/300/400?1",
+      thumbnail: "https://picsum.photos/400/600?1",
       url: "https://example.com"
     },
     {
       id: 2,
       title: "Test Video 2",
-      thumbnail: "https://picsum.photos/300/400?2",
+      thumbnail: "https://picsum.photos/400/600?2",
       url: "https://example.com"
     }
   ]);
