759c140c2c412b50008b7f10709963ef5e6ea8eb
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-05 08:02:31 +0700

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,8 +1,4 @@
-export default async function handler(req, res) {
-  try {
-    await fetch("https://asupan01.vercel.app/api/videos")
-    return res.status(200).json({ success: true })
-  } catch (e) {
-    return res.status(500).json({ error: e.message })
-  }
+// api/cron.js
+export default function handler(req, res) {
+  res.status(200).json({ status: "OK" });
 }
