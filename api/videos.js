36035a6a52f2d5509f0e56e88f9aa4c417380718
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-05 08:07:50 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,9 +1,16 @@
 export default async function handler(req, res) {
   try {
-    const apiKey = process.env.VIDARA_KEY;
+    const response = await fetch(
+      `https://api.vidara.so/v1/video/list?api_key=${process.env.API_KEY}&limit=20`
+    );
 
-    const response = await fetch(`https://vidara.so/v/api?key=${apiKey}`);
     const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Failed fetch" });
+  }
+}
 
     // sesuaikan dengan struktur API asli
     const list = data.result || data.data || [];
