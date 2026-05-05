b7d52e0d6e33ec4ac18a61f1017b52fa04ba5ff0
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-05 12:29:40 +0700

    Update vidara.js

--- api/vidara.js
+++ api/vidara.js
@@ -42,6 +42,7 @@ export default async function handler(req, res) {
     res.setHeader("Cache-Control", "s-maxage=60");
     res.status(200).json(videos);
     res.setHeader("Access-Control-Allow-Origin", "*");
+    
   } catch (err) {
     console.error("VIDARA ERROR:", err);
     res.status(500).json({ error: "Vidara fetch gagal" });
