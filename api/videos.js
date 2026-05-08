01e98aefad0ab57f85d466bc609a5aa84009500e
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-08 23:39:49 +0700

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -60,7 +60,7 @@ export default async function handler(req, res) {
 
             // LINK VIDEO
             url:
-              `https://vizey.net/v/${video.id}`,
+              `https://vizey.net/e/${video.id}`,
 
             source:
               "vizey"
