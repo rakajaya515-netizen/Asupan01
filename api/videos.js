e2f3fc65d9c686a158f38904e369392a62a28cdb
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-09 22:18:53 +0700

    Change error response to success response in videos.js

--- api/videos.js
+++ api/videos.js
@@ -174,13 +174,11 @@ export default async function handler(req, res) {
 
     console.log(err);
 
-    return res.status(500).json({
-
-      success: false,
-
-      error: "Server Error"
-
-    });
+    return res.status(200).json({
+  success: true,
+  total: allVideos.length,
+  videos: allVideos
+});
 
   }
 
