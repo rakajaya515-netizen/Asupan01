6e0b35c1638affbb11183f813b2795ca5e880220
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-10 20:36:32 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,32 +1,27 @@
 const grid = document.getElementById("videos");
 const loading = document.getElementById("loading");
 
-let batch = 1;
-let isLoading = false;
-let finished = false;
+let currentPage = 1;
+
+let loadingNow = false;
+let hasNext = true;
 
 const used = new Set();
 
 async function loadVideos() {
 
-  if (isLoading || finished) return;
+  if (loadingNow || !hasNext) return;
 
-  isLoading = true;
+  loadingNow = true;
 
   loading.innerHTML = "Loading videos...";
 
   try {
 
-    const res = await fetch(`/api/videos?batch=${batch}`);
+    const res = await fetch(`/api/videos?page=${currentPage}`);
     const data = await res.json();
 
     const videos = data.videos || [];
-
-    if (videos.length === 0) {
-      finished = true;
-      loading.innerHTML = "No more videos";
-      return;
-    }
 
     videos.forEach(video => {
 
@@ -54,9 +49,15 @@ async function loadVideos() {
 
     });
 
-    batch++;
+    hasNext = data.hasNext;
 
-    loading.innerHTML = "";
+    currentPage++;
+
+    if (!hasNext) {
+      loading.innerHTML = "All videos loaded";
+    } else {
+      loading.innerHTML = "";
+    }
 
   } catch (err) {
 
@@ -64,13 +65,11 @@ async function loadVideos() {
 
   }
 
-  isLoading = false;
+  loadingNow = false;
 }
 
-// pertama kali
 loadVideos();
 
-// infinite scroll
 window.addEventListener("scroll", () => {
 
   const {
@@ -79,7 +78,9 @@ window.addEventListener("scroll", () => {
     clientHeight
   } = document.documentElement;
 
-  if (scrollTop + clientHeight >= scrollHeight - 500) {
+  if (
+    scrollTop + clientHeight >= scrollHeight - 500
+  ) {
     loadVideos();
   }
 
