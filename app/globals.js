86e50aedfe84628789b8bbc9c05ba1129ec7a19b
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 10:19:00 +0700

    Create globals.js

---
+++ app/globals.js
@@ -0,0 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+body {
+  background: black;
+  color: white;
+  overflow-x: hidden;
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
