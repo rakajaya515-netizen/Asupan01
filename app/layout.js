70841a140c9f2850a48f2369195a24b78dd6501f
rakajaya515-netizen <rakajaya515@gmail.com>
2026-05-11 11:11:16 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,13 @@
-import "./globals.css"
+import "./globals.css";
 
 export const metadata = {
   title: "Asupanmu",
-  description: "Streaming Video",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
